3f6db1c75f00b52f5f2fe5e760e0b562f6484476
adnan s hussain <adnanshussain@gmail.com>
2016-10-08 18:04:19 +0400

    - Updated Config mechanism

--- webapp/config.py
+++ webapp/config.py
@@ -1,6 +1,10 @@
-import os
+import os, configparser
 
-# basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+ini = configparser.ConfigParser()
+ini.read(os.path.join(basedir,  'config.ini'))
+
 
 class _ConfigBase:
     ###############################
@@ -16,7 +20,7 @@ class _ConfigBase:
     ### Flask Configs           ###
     ###############################
     HOST = None
-    SECRET_KEY = "secret key for session usage"
+    SECRET_KEY = "Hs-Sk-321$765*"
 
 class _DevConfig(_ConfigBase):
     # HOST = "0.0.0.0"
@@ -24,16 +28,15 @@ class _DevConfig(_ConfigBase):
     DEBUG = True
     #TEMPLATES_AUTO_RELOAD = True
     #EXPLAIN_TEMPLATE_LOADING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sa123$@localhost:5432/haystaxs'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://gpadmin:S3cr3t!@24.150.86.245:5432/haystack'
+    SQLALCHEMY_DATABASE_URI = ini['DEV']['DB URL']
     SQLALCHEMY_ECHO = False
 
 class _ProdConfig(_ConfigBase):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://gpadmin:S3cr3t!@192.168.1.240:5432/haystack'
+    SQLALCHEMY_DATABASE_URI = ini['PROD']['DB URL']
 
 _configs = {
-    'Dev': _DevConfig(),
-    'Prod': _ProdConfig()
+    'DEV': _DevConfig(),
+    'PROD': _ProdConfig()
 }
 
 _ENVAR_HAYSTAXS_CONFIG = 'HAYSTAXS_CONFIG'
